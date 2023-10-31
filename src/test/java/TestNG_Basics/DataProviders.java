2b43cc8b840ddb1fa5159eb00086ebfe49c0b1fb
vivekanandd1 <vivekanandd1@live.com>
2023-10-31 17:05:48 +0530

    DataProviders

---
+++ src/test/java/TestNG_Basics/DataProviders.java
@@ -0,0 +1,29 @@
+package TestNG_Basics;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DataProviders {
+	
+	@Test (dataProvider = "logincred" ,dataProviderClass = DataOcean.class)
+	public void Login(String Username,String Password) throws Throwable{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.findElement(By.name("username")).sendKeys(Username);
+		driver.findElement(By.name("password")).sendKeys(Password);
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Thread.sleep(3000);
+		driver.quit();
+		
+	}
+	
+	
+
+}
